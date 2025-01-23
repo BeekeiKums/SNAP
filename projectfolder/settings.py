4d4bb0034132bf8794fd85c02b0039fdb16da7b4
cococoolbeana@gmail.com <your.name@domain.com>
2025-01-23 11:52:35 +0800

    Reapply "update 4:46"
    This reverts commit 974be745f0d2eb3325169277554f5cf4ed9890e2.

--- projectfolder/settings.py
+++ projectfolder/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 INSTALLED_APPS = [
     # ...existing code...
-    'main',  # Add your app to the installed apps
+    'main',  # Ensure your app is added to the installed apps
 ]
 
 # ...existing code...
@@ -12,4 +12,26 @@ INSTALLED_APPS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# ...existing code...
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 # ...existing code...
